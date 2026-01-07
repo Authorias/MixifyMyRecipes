3708c28fbb84431adf6366049e2e905cf766ef7f
Authorias <remco.brouwer@outlook.com>
2026-01-07 18:49:52 +0100

    Added JWT stuff

--- src/app/Http/Controllers/Api/ApiController.php
+++ src/app/Http/Controllers/Api/ApiController.php
@@ -5,15 +5,17 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 
 use App\Http\Controllers\Api\Converters\JsonModelConverter;
+use App\Http\Controllers\Api\Converters\DefaultJsonModelConverter;
 
-class ApiController extends Controller {
+
+abstract class ApiController extends Controller {
     private JsonModelConverter $jsonConverter;
 
     protected function getConverter() : JsonModelConverter {
         return $this->jsonConverter;
     }
 
-    public function __construct(JsonModelConverter $jsonConverter) {
-        $this->jsonConverter = $jsonConverter;
+    public function __construct(?JsonModelConverter $jsonConverter = null) {
+        $this->jsonConverter = $jsonConverter ?? new DefaultJsonModelConverter();
     }
 }
