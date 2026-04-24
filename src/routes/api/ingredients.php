0dc3baa367f72109d1d33aa1ca257b9a072338b9
Remco Brouwer <remco.brouwer@proton.me>
2026-04-24 09:29:55 +0200

    Small style changes

--- src/routes/api/ingredients.php
+++ src/routes/api/ingredients.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Api\IngredientController;
-use App\Http\Controllers\Api\UnitController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('ingredients')->name('api.ingredients.')->group(function (): void {
@@ -19,25 +18,6 @@ Route::prefix('ingredients')->name('api.ingredients.')->group(function (): void 
         ->middleware(['auth', 'throttle:api'])
         ->name('update');
     Route::delete('{id}', [IngredientController::class, 'delete'])
-        ->middleware(['auth', 'throttle:api'])
-        ->name('delete');
-});
-
-Route::prefix('units')->name('api.units.')->group(function (): void {
-    Route::get('/', [UnitController::class, 'index'])
-        ->middleware('throttle:api')
-        ->name('index');
-    Route::get('{id}', [UnitController::class, 'get'])
-        ->middleware('throttle:api')
-        ->name('get');
-
-    Route::post('/', [UnitController::class, 'add'])
-        ->middleware(['auth', 'throttle:api'])
-        ->name('add');
-    Route::put('{id}', [UnitController::class, 'update'])
-        ->middleware(['auth', 'throttle:api'])
-        ->name('update');
-    Route::delete('{id}', [UnitController::class, 'delete'])
         ->middleware(['auth', 'throttle:api'])
         ->name('delete');
 });
